ec7043542dfde23777736e0c3c5dc585f5e2514c
danyllosiqueira <danyllosiqueira@gmail.com>
2021-09-09 02:58:11 -0300

    completando o projeto com base na aula

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# dio-projeto2
+# Bootcamp Santander (Fullstack Developer) | Digital Inovation One
+
+dio-projeto2: Desenvolvendo um sistema de gerenciamento de pessoas em API REST com Spring Boot
+
---
+++ src/main/java/one/digitalinnovation/personapi/enums/PhoneType.java
@@ -0,0 +1,24 @@
+package one.digitalinnovation.personapi.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+public enum PhoneType {
+
+    HOME("Home"),
+    MOBILE("Mobile"),
+    COMMERCIAL("Commercial");
+
+    private final String description;
+
+	public String getDescription() {
+		return description;
+	}
+	
+	private PhoneType(String description) {
+		this.description = description;
+	}
+    
+    
+}
